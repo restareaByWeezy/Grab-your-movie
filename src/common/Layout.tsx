74bb7865871035dc7087d238a3cc1867614d7177
restareaByWeezy <64529155+restareaByWeezy@users.noreply.github.com>
2022-06-14 20:36:05 +0900

    💄 Style: modal bg blur

--- src/common/Layout.tsx
+++ src/common/Layout.tsx
@@ -1,14 +1,19 @@
 import { Outlet } from 'react-router-dom'
+import { useRecoilValue } from 'recoil'
+import { cx } from 'styles'
 
 import styles from './Layout.module.scss'
 
+import { modalOpenAtom } from './atom/Atom'
 import Header from './components/Header'
 import Navbar from './components/Navbar'
 
 const Layout = () => {
+  const modalOpen = useRecoilValue<boolean>(modalOpenAtom)
+
   return (
     <div className={styles.background}>
-      <div id='container' className={styles.container}>
+      <div id='container' className={cx(styles.container, { [styles.isOpen]: modalOpen })}>
         <div className={styles.wrapper}>
           <Header />
           <main>
